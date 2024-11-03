afd24859886a72b986f6d11cc7ce9deeb406b98e
AkshayGaigole <vickygaigole@gmail.com>
2024-11-03 08:12:50 +0530

    completed whole of scala

--- src/main/scala/first.scala
+++ src/main/scala/first.scala
@@ -2,42 +2,90 @@ object first {
 
   def main(args: Array[String]):Unit={
 
-    val a=10
-    println("value of a is :"+a)
+//    val a=10
+//    println("value of a is :"+a)
+//
+//    var b=10
+//    b=20
+//    b=30
+//    b=50
+//    b=90
+//
+//    println("value of b is :"+b)
+//
+//    var c=10
+//    println("value of c is :"+c)
+//
+//    if(c%2==0){
+//      println("c is an even number")
+//
+//    }
+//    else{
+//      println("c is not an even number")
+//    }
+//
+//    for (i<- 1 to 10 by 1){
+//
+//      println(i)
+//    }
+//
+//    println("display 1 to 10 but by 3 steps")
+//
+//    for(i<- 1 to 10 by 3){
+//      println(i)
+//    }
+//
+//    for(i<-1 until 10 by 3){
+//      println(i)
+//    }
 
-    var b=10
-    b=20
-    b=30
-    b=50
-    b=90
+//    println("using while loop")
+//    var num1=1
+//    while(num1 <= 10){
+//      println(num1)
+//      num1=num1+1
+//    }
+//
 
-    println("value of b is :"+b)
+//      var arr=Array(10,20,30,40,50,60,70,80,90,100)
+//      println(arr(0))
+//
+//      arr.foreach(println)
+//
+//      val len =arr.length
+//      println("length of array is :"+len)
+//
+//      for(i <- 0 until len ){
+//        println(arr(i))
+//
+//      }
 
-    var c=10
-    println("value of c is :"+c)
 
-    if(c%2==0){
-      println("c is an even number")
+        /*
+        var tup=(10,20,10.5,true)
+        println("2nd element of the tuple is :"+tup._2)
 
-    }
-    else{
-      println("c is not an even number")
-    }
+        var c=0
+        for(i<-tup.productIterator){
+          println(i)
+          c=c+1
+        }
+         */
 
-    for (i<- 1 to 10 by 1){
 
-      println(i)
-    }
+        var mylist=List(10,20,30,40,50,60,70,80,90,100)
+//        println(mylist(1))
+//
+//        mylist.foreach(println)
 
-    println("display 1 to 10 but by 3 steps")
+        for(i <- 0 until mylist.length){
+          println(mylist(i))
+        }
+        println(" ")
+        println(mylist.head)
+        println(mylist.tail)
 
-    for(i<- 1 to 10 by 3){
-      println(i)
-    }
 
-    for(i<-1 until 10 by 3){
-      println(i)
-    }
 
 
   }
